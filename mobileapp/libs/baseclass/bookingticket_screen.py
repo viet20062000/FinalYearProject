2596fe7ba108e0b0e4472b40605907647aeafa97
vietlqgch18109 <69781453+vietlqgch18109@users.noreply.github.com>
2021-04-26 22:04:05 +0700

    change booking screen layout and logic

--- mobileapp/libs/baseclass/bookingticket_screen.py
+++ mobileapp/libs/baseclass/bookingticket_screen.py
@@ -4,18 +4,21 @@ from kivy.uix.widget import Widget
 from datetime import date
 import sqlite3
 
-from kivymd.uix.list import TwoLineAvatarListItem
+from kivymd.uix.list import TwoLineAvatarIconListItem
 class CinebotBookingTicketScreen(MDScreen):
 	def on_enter(self):
-		today=date.today()
-		conn=sqlite3.connect(r"libs\cinema.db")
-		cursor=conn.cursor()
-		sql=f"SELECT film_id, image, title, overview from film where release_date < '{today}'order by release_date desc limit 5"
-		cursor.execute(sql)
-		records=cursor.fetchall()
-		for row in records:
-			self.ids.container.add_widget(CinebotTwoLineLeftAvatarItem(image=row[1], text=row[2],film_id=str(row[0])))
-class CinebotTwoLineLeftAvatarItem(TwoLineAvatarListItem):
+		if len(self.ids.container.children) ==0 :
+			today=date.today()
+			conn=sqlite3.connect(r"libs\cinema.db")
+			cursor=conn.cursor()
+			sql=f"SELECT film_id, image, title, overview from film where release_date < '{today}'order by release_date desc limit 5"
+			cursor.execute(sql)
+			records=cursor.fetchall()
+			for row in records:
+				self.ids.container.add_widget(CinebotTwoLineAvatarIconItem(image=row[1], text=row[2],film_id=str(row[0])))
+		else:
+			return 0
+class CinebotTwoLineAvatarIconItem(TwoLineAvatarIconListItem):
     image = StringProperty()
     title = StringProperty()
     overview= StringProperty()
