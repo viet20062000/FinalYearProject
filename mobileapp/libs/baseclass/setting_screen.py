b6f6143cd2cbfd0d8633f79a150ba5e5162a78bc
vietlqgch18109 <69781453+vietlqgch18109@users.noreply.github.com>
2021-04-27 09:48:02 +0700

    change dataset and update chat bot

--- mobileapp/libs/baseclass/setting_screen.py
+++ mobileapp/libs/baseclass/setting_screen.py
@@ -10,16 +10,20 @@ class CinebotSettingScreen(MDScreen):
     def on_pre_enter(self):
         if not self.created:
             items = [
-                "Sign Out"
+                "Sign Out",
+                "About Us - Cinebot team",
+                "Change background"
             ]
             for i in items:
                 list_obj = OneLineListItem(
                     text=i, divider="Inset", font_style="H6"
                 )
-                list_obj.bind(on_release=self.logout)
+                list_obj.bind(on_release=lambda wdt:self.multi_function(wdt.text))
                 self.ids._list.add_widget(list_obj)
             self.created = True
 
-    def logout(self, obj):
-        self.parent.parent.parent.parent.current = "cinebot login screen"
-        self.parent.parent.parent.ids.nav_bar.set_current(-1)
+    def multi_function(self, text):
+        if text=="Sign Out":
+            self.parent.parent.parent.parent.current = "cinebot login screen"
+            self.parent.parent.parent.ids.nav_bar.set_current(-1)
+        
