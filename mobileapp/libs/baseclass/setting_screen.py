cab4a5a916d68422b77d96eac3c9a4dd1f28662c
vietlqgch18109 <69781453+vietlqgch18109@users.noreply.github.com>
2021-05-09 23:25:44 +0700

    create confirmation dialog for sign out

--- mobileapp/libs/baseclass/setting_screen.py
+++ mobileapp/libs/baseclass/setting_screen.py
@@ -2,11 +2,27 @@ from kivy.properties import BooleanProperty
 
 from kivymd.uix.list import OneLineListItem
 from kivymd.uix.screen import MDScreen
-
-
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 class CinebotSettingScreen(MDScreen):
+    dialog=None
     created = BooleanProperty(False)
-
+    def show_logout_alert(self):
+        self.dialog = MDDialog(
+            title='LOG OUT',
+            text="Are you sure?",
+            buttons=[
+                MDFlatButton(
+                    text="CANCEL", font_style="H6",on_release=self.dialog_close
+                ),
+                MDFlatButton(
+                    text="LOG OUT", font_style="H6",on_release=self.logout
+                ),
+            ],
+        )
+        self.dialog.open()
+    def dialog_close(self, *args):
+        self.dialog.dismiss(force=True)
     def on_pre_enter(self):
         if not self.created:
             items = [
@@ -18,15 +34,19 @@ class CinebotSettingScreen(MDScreen):
                 list_obj = OneLineListItem(
                     text=i, divider="Inset", font_style="H6"
                 )
-                list_obj.bind(on_release=lambda wdt:self.multi_function(wdt.text))
+                list_obj.bind(on_release=lambda wdt:self.show_logout_alert())
                 self.ids._list.add_widget(list_obj)
             self.created = True
-
+    def logout(self, *args):
+        self.dialog_close()
+        self.parent.parent.parent.parent.current = "cinebot login screen"
+        self.parent.parent.parent.ids.nav_bar.set_current(-1)
     def multi_function(self, text):
         if text=="Sign Out":
-            self.parent.parent.parent.parent.current = "cinebot login screen"
-            self.parent.parent.parent.ids.nav_bar.set_current(-1)
+            self.show_logout_alert()
+            # self.parent.parent.parent.parent.current = "cinebot login screen"
+            # self.parent.parent.parent.ids.nav_bar.set_current(-1)
         if text=="Change background":
             print("hello")
-            self.parent.parent.parent.parent.parent.parent.md_bg_color="gch('#ffffff')"
+            self.parent.parent.parent.parent.parent.parent.md_bg_color='gch("#ffffff")'
         
